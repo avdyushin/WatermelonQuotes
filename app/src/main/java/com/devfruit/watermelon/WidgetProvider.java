ac65448539e48dd925ec382c48b1d10cc7360102
Grigory Avdyushin <avdyushin.g@gmail.com>
2017-11-18 19:38:50 +0100

    Fix many call on screen on

--- app/src/main/java/com/devfruit/watermelon/WidgetProvider.java
+++ app/src/main/java/com/devfruit/watermelon/WidgetProvider.java
@@ -9,44 +9,53 @@ import android.content.IntentFilter;
 
 public class WidgetProvider extends AppWidgetProvider {
 
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            context.startService(new Intent(context, UpdateService.class));
-        }
-    };
+    private BroadcastReceiver receiver;
 
-    private void registerScreenOn(Context context) {
-        try {
+    private void register(Context context, String action) {
+        if (receiver == null) {
+            receiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    context.startService(new Intent(context, UpdateService.class));
+                }
+            };
             context.getApplicationContext().registerReceiver(
-                    receiver, new IntentFilter(Intent.ACTION_SCREEN_ON)
+                    receiver, new IntentFilter(action)
             );
-        } catch (Exception e) {
         }
+    }
+
+    private void unregister(Context context) {
+        if (receiver != null) {
+            context.getApplicationContext().unregisterReceiver(receiver);
+        }
+    }
+
+    private void start(Context context) {
+        context.startService(new Intent(context, UpdateService.class));
+        register(context, Intent.ACTION_SCREEN_ON);
+    }
+
+    private  void stop(Context context) {
+        context.stopService(new Intent(context, UpdateService.class));
+        unregister(context);
     }
 
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
-        context.startService(new Intent(context, UpdateService.class));
-        registerScreenOn(context);
+        start(context);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        start(context);
     }
 
     @Override
 	public void onDisabled(Context context) {
         super.onDisabled(context);
-        context.stopService(new Intent(context, UpdateService.class));
-        try {
-            context.getApplicationContext().unregisterReceiver(receiver);
-        } catch (Exception e) {
-
-        }
-	}
-
-	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-		context.startService(new Intent(context, UpdateService.class));
-		registerScreenOn(context);
+        stop(context);
 	}
 }
