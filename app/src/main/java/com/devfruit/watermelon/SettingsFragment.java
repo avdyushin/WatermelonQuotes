1acb8403494e13f0d1353faafa70a1fb87bbecf2
Grigory Avdyushin <avdyushin.g@gmail.com>
2017-11-23 20:35:00 +0100

    Fix request permissions and crashes

--- app/src/main/java/com/devfruit/watermelon/SettingsFragment.java
+++ app/src/main/java/com/devfruit/watermelon/SettingsFragment.java
@@ -9,9 +9,10 @@ import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
+import android.support.v13.app.FragmentCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
@@ -21,6 +22,9 @@ import java.util.List;
 import java.util.Set;
 
 public class SettingsFragment extends PreferenceFragment {
+
+    private final static String TAG = SettingsFragment.class.getName();
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,12 +50,11 @@ public class SettingsFragment extends PreferenceFragment {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getActivity().getApplicationContext());
         Set<String> selected = prefs.getStringSet("biter_quotes_key", null);
         updateSummary(preference, selected);
-
     }
 
     private void updateSummary(Preference preference, Set<String> values) {
         if (values.isEmpty()) {
-            preference.setSummary("");
+            preference.setSummary(R.string.ru_biter_sum);
         } else {
             preference.setSummary(getNamesFromSelectedValues(values).toString());
         }
@@ -73,43 +76,55 @@ public class SettingsFragment extends PreferenceFragment {
     final static int REQUEST = 1;
 
     private void updateExternalSources(boolean hasStorage, boolean granted) {
-        MultiSelectListPreference help = (MultiSelectListPreference) findPreference("external_sources");
-        Log.d("TAG", "Updated " + help.toString());
-        Log.d("TAG", "Params " + hasStorage + ", " + granted);
+        PreferenceScreen external_sources = (PreferenceScreen) findPreference("external_sources");
+        Log.d(TAG, "Updated " + external_sources.toString());
+        Log.d(TAG, "Params with " + hasStorage + ", " + granted);
         if (hasStorage) {
             if (granted) {
                 ArrayList<QuoteSource> installed = ExternalSourceProvider.userInstalledQuotes();
                 int total = installed.size();
-                help.setTitle(getString(R.string.put_quotes, ExternalSourceProvider.QUOTES_PATH));
-                help.setSummary(getResources().getQuantityString(R.plurals.found_quotes, total));
+                if (total == 0 ) {
+                    external_sources.setTitle(R.string.not_found);
+                } else {
+                    external_sources.setTitle(getResources().getQuantityString(R.plurals.found_quotes, total));
+                }
+                external_sources.setSummary(getString(R.string.put_quotes, ExternalSourceProvider.QUOTES_PATH));
             } else {
-                help.setTitle(R.string.no_permissions);
-                help.setSummary(R.string.requires_external);
+                external_sources.setTitle(R.string.no_permissions);
+                external_sources.setSummary(R.string.requires_external);
             }
         } else {
-            help.setTitle(R.string.no_external_storage);
-            help.setSummary(R.string.requires_external);
+            external_sources.setTitle(R.string.no_external_storage);
+            external_sources.setSummary(R.string.requires_external);
         }
     }
 
     private void checkPermission() {
+
+        Log.d(TAG,"check permissions");
+
         String state = Environment.getExternalStorageState();
         if ( state.equals(Environment.MEDIA_MOUNTED) ||
                 state.equals(Environment.MEDIA_MOUNTED_READ_ONLY) ) {
+
+            Log.d(TAG,"Found card");
 
             String permissions = Manifest.permission.READ_EXTERNAL_STORAGE;
             if (ContextCompat.checkSelfPermission(
                     this.getActivity().getApplicationContext(), permissions
                 ) != PackageManager.PERMISSION_GRANTED) {
 
-                ActivityCompat.requestPermissions(
-                        this.getActivity(), new String[] { permissions }, REQUEST
+                Log.d(TAG,"No permissions, will ask");
+                FragmentCompat.requestPermissions(
+                        this, new String[] { permissions }, REQUEST
                 );
             } else {
+                Log.d(TAG,"Yes, we have");
                 updateExternalSources(true, true);
             }
 
         } else {
+            Log.d(TAG,"NO SD CARD");
             updateExternalSources(false, false);
         }
     }
@@ -119,8 +134,13 @@ public class SettingsFragment extends PreferenceFragment {
         switch (requestCode) {
             case REQUEST: {
                 boolean granted = (
-                        (grantResults.length > 1) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                        (grantResults.length >= 1) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 );
+                if (grantResults.length >= 1) {
+                    Log.d(TAG,"results " + grantResults[0] + ", " + PackageManager.PERMISSION_GRANTED);
+                } else {
+                    Log.d(TAG,"length < 1");
+                }
                 updateExternalSources(true, granted);
                 break;
             }
